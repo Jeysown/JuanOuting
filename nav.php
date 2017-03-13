122933152dcd0b9945c0195c58f5825dc83d988a
Jeysown <jeysownlopez@gmail.com>
2017-03-14 07:44:50 +0800

    commit na itu

--- nav.php
+++ nav.php
@@ -21,11 +21,9 @@
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span> <i class="fa fa-bars"></i>
             </button>
-<<<<<<< HEAD
-              <a class="navbar-brand page-scroll" href="index.php">JuanderPool</a>
-=======
+
               <a class="navbar-brand page-scroll" href="index.php" style="color:#59e7e5;font-family:Aleo;font-weight:600;">JuanderP<img src="assets/images/logo.png" alt="" style="width:40px;display:inline;">L</a>
->>>>>>> 6faf863d54b802d99f505c424e394b5aaf5bb007
+
               <i class="fa fa-search search-show" aria-hidden="true"  onClick="openSearch();visibleSearch();"></i>
                 <div id="searchOffset">
                   <i class="fa fa-search search-header-icon" aria-hidden="true" id="yey"></i><input id="resort1" onkeyup="AutoComplete1()" class="ui-autocomplete-input search-header" placeholder="Search" autocomplete="off" >
