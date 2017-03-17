75d86e120fcb69fdd6c3aab8a06fd2eb3c435a83
Jeysown <jeysownlopez@gmail.com>
2017-03-17 09:43:25 +0800

    color

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
         </div>
         <div class="col-sm-9">
           <center><h1 class="header-title"><img src="assets/images/duck.png" alt="" width="80px" class="hidden-sm hidden-xs">JuanderPool <img src="assets/images/duck.png" alt="" width="80px" style="margin-left:-15px;" class="hidden-sm hidden-xs"></h1></center>
-          <center><h4 class="title-subheader" >Make Booking a Private Pool a Wonderul Experience for Every Filipino </h4></center>
+          <center><h4 class="title-subheader" >Make Booking a Private Pool a Wonderful Experience for Every Filipino </h4></center>
         </div>
       </div>
       <?php include 'search-content.php' ?>
