1b7b39d55b191f48905f62d04c8492ebd2898984
Jeysown <jeysownlopez@gmail.com>
2017-05-26 09:39:59 +0800

    header help

---
+++ help.php
@@ -0,0 +1,152 @@
+<?php include 'nav-payment.php' ?>
+<?php include 'search-content-frontend.php' ?>
+<div class="parent-header-help" style="padding-top: 60px;">
+    <div class="child-header-help"></div>
+  <div class="header-text-help">
+  <center><p class="header-jumbotron-help ">We are here to help you.</p>
+      <i class="fa fa-search search-header-icon-help" aria-hidden="true" id="yey"></i><input id="resort1" onkeyup="AutoComplete1()" class="ui-autocomplete-input search-header-help" placeholder="Search" autocomplete="off" >
+  </center>
+  </div>
+</div>
+  <div class="container" style="padding-top:50px;">
+
+    <div class="r-title">
+      <div class="col-sm-6 deck" >
+        <div class="resort-name">
+          Balinese Villa
+        </div>
+        <div class="resort-address">
+          <p class="r-p"> Calamba, Calabarzon, Philippines</p>
+          <a class="r-map-button" href="#">View on Map</a>
+        </div>
+      </div>
+      <div class="col-sm-6 deck" >
+        <p class="r-p"><span class="r-rating"> <img src="assets/images/duck.png" alt="" width="22px"><img src="assets/images/duck.png" alt="" width="22px"><img src="assets/images/duck.png" alt="" width="22px"><img src="assets/images/duck.png" alt="" width="22px"><img src="assets/images/duck.png" alt="" width="22px"></span></p>
+        <p class="r-p"><span class="r-review">Based on 3 reviews</span></p>
+      </div>
+    </div>
+    <div class="col-xs-12 deck r-overall">
+      <!-- Overview of contents -->
+      <div class="r-title-description">
+        <p>About Us</p>
+      </div>
+      <p>Our Cozy four bed room private villa and social hall has a true filipino feeling.
+          We can only accommodate around 16 persons, 24 can be manageable to sleep and less than 100 to party
+          at the social hall Or swimming and stargazing on a late evening.</p>
+          <!-- Prices -->
+          <div>
+            <div class="r-title-description">
+                <p>Prices</p>
+            </div>
+            <div class="col-xs-6 r-content-description">
+              <p>Half Day - Morning: <b>₱5,000</b></p>
+            </div>
+            <div class="col-xs-6 r-content-description">
+              <p>Half Day - Evening: <b>₱5,000</b></p>
+            </div>
+            <div class="col-xs-6 r-content-description">
+              <p>Whole Day: <b>₱10,000</b></p>
+            </div>
+          </div>
+          <!-- Check in Check out -->
+      <div>
+        <div class="r-title-description">
+            <p>Check In / Check Out</p>
+        </div>
+        <div class="col-xs-6 r-content-description">
+          <p>Check In: <b>8AM</b></p>
+        </div>
+        <div class="col-xs-6 r-content-description">
+          <p>Check Out: <b>6AM</b></p>
+        </div>
+      </div>
+      <!-- Check in Check out -->
+      <!-- Bathrooms, Bedrooms, and Beds -->
+      <div>
+        <div class="r-title-description">
+            <p>Bathrooms, Bedrooms, and Beds</p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/bathroom.png" alt=""  class="icon-filter">Bathrooms: <b>4</b></p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/bedroom.png" alt=""  class="icon-filter">Bedrooms: <b>4</b></p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/bed.png" alt=""  class="icon-filter">Beds: <b>4</b></p>
+        </div>
+      </div>
+      <!-- Bathrooms, Bedrooms, and Beds -->
+      <!--What they have  -->
+    <div>
+        <div class="r-title-description">
+            <p>What we offer</p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/kiddie.jpg" alt=""  class="icon-filter">Kiddie Pool:<b> 3ft</b></p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/adult_pool.png" alt="" class="icon-filter">Adult Pool:<b> 6ft</b></p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/jacuzzi.png" alt=""  class="icon-filter">Jacuzzi</p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/billiard.png" alt=""  class="icon-filter">Billiards</p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/videoke.png" alt=""  class="icon-filter">Videoke</p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/kitchen.png" alt=""  class="icon-filter">Kitchen</p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/griller2.png" alt=""  class="icon-filter">Griller</p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/hall.png" alt=""  class="icon-filter">Function Hall</p>
+        </div>
+        <div class="col-xs-6 col-sm-4 r-content-description">
+          <p><img src="assets/images/icons/wifi.png" alt=""  class="icon-filter">Wifi</p>
+        </div>
+      </div>
+      <!--What they have  -->
+      <!-- Description -->
+      <div>
+        <div class="r-title-description" >
+            <p>Know more about us</p>
+        </div>
+        <div class="col-xs-12 r-content-description">
+          <p>Clean and open space good for party, wedding reception, baptismal, debut, team building or just plain family gathering or friends get together. As listed 16 pax to comfortably sleep on the listed price in the excess of 16 pax shall be subject to additional fee, we can also accommodate less 100 people to party at the social hall but subject to confirmation and additional fee. The spacious pavillion with good air circulation will give you a relaxing feeling.</p>
+        </div>
+      </div>
+      <!-- Description -->
+      <!-- House Rules -->
+      <div>
+        <div class="r-title-description" >
+            <p>House Rules</p>
+        </div>
+        <div class="col-xs-12 r-content-description">
+          <p>We request a non smoking on the bedroom and toilet and bathrooms. As much as possible please keep our place clean so others can also enjoy our place.
+            Smoking is allowed only at the social hall provided that the cigarette buts will only end up at the ash tray.</p>
+          <p>No drinking or eating inside the swimming pool.</p>
+          <p>We have provide beautiful ceramic vases to contemplate the resort and tropical feel in our rest house please do take good care of them any damage will be charge accordingly, we do not provide towels,blankets and toiletries please do bring your own.</p>
+          <p>A care taker will always be present to assist you but please do respect that he is there to check if everthing is in good condition nothing more nothing less</p>
+          <p>Please keep in mind that the karaoke machine is provided free of charge but if the microphone is damage by you or your group you will be charge to the security deposit</p>
+        </div>
+      </div>
+      <!-- House Rules -->
+      <!-- Other Charges -->
+      <div>
+        <div class="r-title-description" >
+            <p>Other Charges</p>
+        </div>
+        <div class="col-xs-12 col-sm-6 r-content-description">
+          <p>LPG: <b>Php300.00/22hr ; Php150.00/day or night </b></p>
+        </div>
+      </div>
+      <!-- Other Charges -->
+
+    </div>
+  </div>
+        <?php include 'footer-payment.php' ?>
